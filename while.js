004f33119f6c4750c987cf4110b745a25ea49302
tofazzal19 <rana.ict337@gmail.com>
2025-07-30 15:35:02 +0600

    loop2

---
+++ while.js
@@ -0,0 +1,57 @@
+/*let i = 0;
+while(i <= 60){
+    console.log('I will learn java daily 6 hour')
+}*/
+
+
+/*let num = 81;
+let sum = 0;
+
+while(num <= 131 ){
+    if(num % 2 == 0){
+        sum +=num;
+      
+    }
+    num++
+    
+}
+
+ console.log(sum)
+ */
+/*
+
+let num1 = 81;
+let sum1 = 0;
+
+while(num1 <= 131 ){
+    if(num1 % 2 !== 0){
+        sum1 +=num1;
+       
+    }
+    num1++
+    
+}
+
+console.log(sum1);
+*/
+/*
+let num =5
+let i = 1;
+while(i <= 10) {
+
+      console.log(`${num} x ${i}= ${num *i}`)
+ 
+i++;
+
+}
+*/
+/* }*/
+
+
+let i = 21;
+while(i >= 15){
+    console.log(i);
+    i--;
+}
+
+
