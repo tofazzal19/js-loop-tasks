6a80dd1efdd64366226a270b60008e466d5f03fc
tofazzal19 <rana.ict337@gmail.com>
2025-07-30 13:52:13 +0600

    loop1

---
+++ for.js
@@ -0,0 +1,5 @@
+
+for(i=1;  i<=60; i++){
+
+    console.log('I will invest at least 6 hrs every single day for next 60 days!')
+}
---
+++ oddeven.js
@@ -0,0 +1,26 @@
+for(i =61; i<= 100; i++){
+    if(i % 2 !== 0){
+      console.log(i);
+
+    }
+   
+}
+
+for(n = 78; n <= 98; n++){
+    if(n % 2 == 0){
+        console.log(n);
+    }
+
+}
+
+
+ num =9
+for(i = 1; i <= 10; i++){
+
+      console.log(`${num} x ${i}= ${num *i}`)
+} 
+
+
+for(i = 81;i >=65;i--){
+    console.log(i);
+}
